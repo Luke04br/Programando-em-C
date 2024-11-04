da9e65cedd91e970031d2f433e4bd26346de0bd4
Lucas Conceição Ricardo <161897897+Luke04br@users.noreply.github.com>
2024-11-04 18:01:51 -0300

    Update cap7.c

--- 2°ciclo/capítulos/cap7.c
+++ 2°ciclo/capítulos/cap7.c
@@ -84,13 +84,23 @@ int main() {
     Fa a um menu com uma opcao para saida do programa.
 		estrutura: nome, end, cidade, estado, cep
 */
-int main() {
-    char Resposta;
-    do {
+struct info{
+	char nome[10], end[50], cidade[15], estado[3], cep[9];
+};
 
-    printf("Deseja inserir novos valores? ");
-    scanf(%c,&Resposta);
-    }while(Resposta!='s' && Resposta!='S');
+void entrada(){
+}
+
+void impressao(struct info dados[4]){
+}
+int main() {
+	struct info dados[4];
+	char Resposta;
+	do {
+
+		printf("Deseja inserir novos valores? ");
+    		scanf(%c,&Resposta);
+    	}while(Resposta!='s' && Resposta!='S');
     return 0;
 }
 #endif//ex2
@@ -106,78 +116,39 @@ struct Calendario{
         int dia, mes, ano;
 };
 
-int meses(struct Calendario DataI, struct Calendario DataII){
-	switch(){
-		case 1:
-			return 0;
-		break;
-		
-		case 2:
-			if(bissexto() == 366)
-				return 29
-			else return 28
-		break;
-		
-		case 3:
-			return 31;
-		break;
-		
-		case 4:
-			return 30;
-		break;
-		
-		case 5:
-			return 31;
-		break;
-		
-		case 6:
-			return 30;
-		break;
-		
-		case 7:
-			return 31;
-		break;
-		
-		case 8:
-			return 31;
-		break;
-		
-		case 9:
-			return 30;
-		break;
-		
-		case 10:
-			return 31;
-		break;
-		
-		case 11:
-			return 30;
-		break;
+int meses(int ano, int mes){
+	if(mes == 1) return 0;
+	if(mes == 2) {
+		if(bissexto(ano) == 366)
+			return 29
+		else return 28
 	}
+	if (mes == 4 || mes == 6 || mes == 9 ||mes == 11 ) return 30;
+	if(mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) return 31;
 }
 
-int bissexto(int x){
-	if((x%4 == 0 && x %100 != 0) || x%400)
+int bissexto(int ano){
+	if((ano % 4 == 0 && ano % 100 != 0) || ano % 400)
             return 366;
         else
             return 365;
 }
 
 unsigned int Calculo(struct Calendario DataI, struct Calendario DataII) {
-    int x, SomaI = 0, SomaII = 0;
+    int ano, mes, SomaI = 0, SomaII = 0;
 	
-//calcula so dias totais da DataI
-	for(x = 0; x < DataI.ano; x++) 
-        	SomaI += bissexto();
-	for(x = 0; x < DataI.mes; x++)
-		SomaI += meses();
+//calcula os dias totais da DataI
+	for(ano = 0; ano < DataI.ano; ano++) 
+        	SomaI += bissexto(ano);
+	for(mes = 1; mes < DataI.mes; mes++)
+		SomaI += meses(ano, mes);
 	Soma += DataI.dia;
 	
-//calcula so dias totais da DataII
-	for(x = 0; x < DataII.ano; x++) 
-        	SomaII += bissexto();
-	for(x = 0; x < DataII.mes; x++)
-		SomaII += meses();
+//calcula os dias totais da DataII
+	for(ano = 0; ano < DataII.ano; ano++) 
+        	SomaII += bissexto(ano);
+	for(mes = 1; mes < DataII.mes; mes++)
+		SomaII += meses(ano, mes);
 	SomaII += DataII.dia;
 	
 	return SomaI - SomaII
@@ -187,24 +158,31 @@ int main(){
     char Resposta;
     struct Calendario DataI, DataII;
     do {
-        printf("Insira o ano da data 1ª data: ");
-        scanf(" %d",&DataI.ano);
-        printf("Insira o mes da data 1ª data: ");
-        scanf(" %d",&DataI.mes);
-        printf("Insira o dia da data 1ª data: ");
-        scanf(" %d",&DataI.dia);
+        printf("Insira o ano da 1ª data: ");
+        scanf("%d",&DataI.ano);
+	printf("Insira o mes da 1ª data: ");
+	do{
+        	scanf("%d",&DataI.mes);
+		if(DataI.mes>12 || DataI.mes<1)
+			printf("O mês deve ser um valor entre 1 e 12: ");
+	}while(DataI.mes>12 || DataI.mes<1);
+	do{
+        printf("Insira o dia da 1ª data: ");
+        scanf("%d",&DataI.dia);
+	}while();
 
-        printf("Insira o ano da data 2ª data: ");
+        printf("Insira o ano da 2ª data: ");
         scanf(" %d",&DataII.ano);
-        printf("Insira o mes da data 2ª data: ");
+        printf("Insira o mes da 2ª data: ");
         scanf(" %d",&DataII.mes);
-        printf("Insira o dia da data 2ª data: ");
+        printf("Insira o dia da 2ª data: ");
         scanf(" %d",&DataII.dia);
 
 	printf("As datas tem %u dias de diferença", Calculo(DataI, DataII));
 
-    printf("Deseja inserir novos valores? (s/n)");
-    scanf("%c",&Resposta);
+	printf("Deseja inserir novos valores? (s/n)");
+	scanf("%c",&Resposta);
+	getchar();
     }while(Resposta!='s' && Resposta!='S');
     return 0;
 }
@@ -221,9 +199,68 @@ int main(){
         Calculo do IMC = peso/(altura*altura).
         estrutura: nome, peso, altura
 */
+struct dados{
+	char nome[10];
+	float peso, altura;
+};
+
+int Menu() {
+	int Resposta;
+	system("cls");
+	printf("*-------------Menu-------------*\n);
+	printf("1 - receber todos os dados"\n);
+	printf("2 - imprime todos os dados"\n);
+	printf("3 - calcula o IMC de todas as pessoas\n");
+	printf("4 - sair\n");
+	printf("Escolha a opção que deseja: ");
+	scanf("%d".&Resposta);
+	getchar();
+	system("cls");
+	return Resposta;
+}
+
 int main(){
-    char Resposta;
-    do {
+	struct dados pessoas[4];
+	int x;
+	setlocale(LC_ALL,"");
+	
+	for(;;){
+		switch(Menu()){
+			case 1:
+				printf("*-------------Entrada-------------*");
+				for(x = 0; x < 4; x++) {
+					printf("\nDigite o nome da xª pessoa: ", x + 1);
+					gets(pessoas[x].nome);
+					printf("\nDigite o peso de %s: ", pessoas[x].nome);
+					scanf("%f",&pessoas[x].peso);
+					printf("\nDigite a altura de %s: ", pessoas[x].nome);
+					scanf("%f",&pessoas[x].altura);
+				}
+				system("pause");
+			break;
+
+			case 2:
+				printf("*-------------Listagem-------------*");
+				for(x = 0; x < 4; x++) {
+					printf("xº:\n")
+					printf("\nNome: %s", pessoas[x].nome);
+					printf("\nPeso: %f", pessoas[x].peso);
+					printf("\nAltura: %f\n", pessoas[x].peso);
+					}
+			break;
+
+			case 3:
+				printf("*-------------Cálculo-------------*");
+			break;
+
+			case 4:
+				printf("*-------------Saindo-------------*");
+				exit(0);
+			break;
+		}
+	}
+}
+	
 
     printf("Deseja inserir novos valores? ");
     scanf(%c,&Resposta);
